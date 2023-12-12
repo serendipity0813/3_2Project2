41923094dc32349e5539e05a8b372e4d8e1c600f
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2023-12-12 19:27:47 +0900

    Status UI Update
    캐릭터 status 나타내는 UI 화면만 제작 / 연결이나 버튼 동작 등은 아직 미구현

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [SerializeField][Range(0f, 1f)] private float musicVolume;
+
+    private AudioSource musicAudioSource;
+    public AudioClip musicClip;
+
+    private void Awake()
+    {
+        instance = this;
+        musicAudioSource = GetComponent<AudioSource>();
+        musicAudioSource.volume = musicVolume;
+        musicAudioSource.loop = true;
+
+    }
+
+    private void Start()
+    {
+        ChangeBackGroundMusic(musicClip);
+    }
+
+    public static void ChangeBackGroundMusic(AudioClip music)
+    {
+        instance.musicAudioSource.Stop();
+        instance.musicAudioSource.clip = music;
+        instance.musicAudioSource.Play();
+    }
+}
